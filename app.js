bd872c66693983510ebb98f5326503b988b28f74
Folawewo Anthony <famwallexie@gmail.com>
2023-02-26 16:46:04 +0100

    connected database

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+require('./services/mongo');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 module.exports = app;
