9129b4e2accc97d222e202b1e331109b69d6857d
Folawewo Anthony <famwallexie@gmail.com>
2023-02-28 22:57:55 +0100

    increase the timeout limit

--- services/mongo.js
+++ services/mongo.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 
 mongoose.connect('process.env.MONGO_URL', () => {
-    console.log('Database Connected')
-})
+  console.log('Database Connected');
+  serverSelectionTimeoutMS: 30000; // Increase timeout to 30 seconds
+});
